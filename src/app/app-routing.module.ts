6ce1fcc5df8b4e4ac576b90ff2f47d4ffdaaa150
Dawid Ciochoń <Dawid.Ciochon@cdn.com.pl>
2020-07-10 14:20:09 +0200

    Changes after day5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import {  NavbarComponent} from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'create-account', component: CreateAccountComponent},
+  { path: 'navbar', component: NavbarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
